1a7e7e56970129ffdafe4424a8effdfd06a6872c
indianaea <126889057+indianaea@users.noreply.github.com>
2024-04-07 21:58:21 +0000

    vgg

---
+++ Lesson 12/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import TeamClock from "./TeamClock";
+import TeamObject from "./TeamObject";
+import GameReviews from "./GameReviews";
+
+export default function Home() {
+  return (
+    <>
+      {/* <TeamClock /> */}
+      {/* <TeamObject /> */}
+      <GameReviews />
+    </>
+  );
+}
